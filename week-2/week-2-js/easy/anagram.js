944a880171f454b8d18b160b12e5644da091164d
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-12 15:05:04 +0530

    week-2-js-easy solution

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,32 @@
 */
 
 function isAnagram(str1, str2) {
+  
+  const st1 = str1.toLowerCase();
+  const st2 = str2.toLowerCase();
+  if(st1.length!=st2.length)return false;
 
+  const letterCount={};
+  for(let i=0;i<st1.length;i++){
+    let ch=st1[i];
+    if(!letterCount[ch]){
+      letterCount[ch]=1;
+    }
+    else{
+      letterCount[ch]+=1;
+    }
+  }
+
+  for(let i=0;i<st2.length;i++){
+    let ch=st2[i];
+    if(!letterCount[ch]){
+      return false;
+    }
+    
+      letterCount[ch]--;
+    
+  }
+  return true;
 }
 
 module.exports = isAnagram;
--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -14,7 +14,22 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let result=[];
+  for(let i=0;i<transactions.length;i++){
+    let currCategory=transactions[i].category;
+    let currPrice=transactions[i].price;
+
+    const isCategoryPresent=result.some(expense=>expense.category===currCategory);
+    if(!isCategoryPresent){
+      result.push({category:currCategory,totalSpent:currPrice});
+    }
+    else{
+      let index = result.findIndex(expense => expense.category === currCategory);
+      // Update the totalSpent value by adding the current price
+      result[index].totalSpent += currPrice;
+    }
+  }
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
